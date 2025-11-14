3081552301f95c748d963976f6625144de5fcf43
Ludovic L'Hours <ludovic.lhours@zenika.com>
2025-11-14 14:04:19 +0100

    Rust server solution

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -5,7 +5,7 @@ use tower_lsp::{Client, LanguageServer, LspService, Server};
 struct TextDocumentItem {
     uri: Url,
     text: String,
-    version: i32
+    version: i32,
 }
 
 #[derive(Debug)]
@@ -18,7 +18,9 @@ impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
-                text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
                 ..ServerCapabilities::default()
             },
             ..InitializeResult::default()
@@ -54,13 +56,73 @@ impl LanguageServer for Backend {
     }
 }
 
+struct Title {
+    range: Range,
+    value: String,
+}
+
 impl Backend {
     async fn on_change(self: &Self, item: TextDocumentItem) {
         let mut diagnostics: Vec<Diagnostic> = Vec::new();
 
+        let mut title: Option<Title> = None;
+        item.text
+            .split("\n")
+            .enumerate()
+            .for_each(|(line, content)| {
+                let mut chars = content.char_indices();
+                let next = chars.next();
+                if next.is_none() {
+                    return
+                }
+                let (offset, c) = next.unwrap();
+                // TODO skip spaces
+                if c == '#' {
+                    // TODO count sharps to check if title or section
+                    if title.is_some() {
+                        diagnostics.push(Diagnostic {
+                            range: Range {
+                                start: Position {
+                                    line: line as u32,
+                                    character: offset as u32,
+                                },
+                                end: Position {
+                                    line: line as u32,
+                                    character: offset as u32+1,
+                                },
+                            },
+                            message: "title already defined".to_string(),
+                            severity: Some(DiagnosticSeverity::ERROR),
+                            ..Diagnostic::default()
+                        });
+                    } else {
+                        title = Some(Title { range: Range {
+                                start: Position {
+                                    line: line as u32,
+                                    character: offset as u32,
+                                },
+                                end: Position {
+                                    line: line as u32,
+                                    character: offset as u32+1,
+                                },
+                            }, value: content[(offset+1) as usize..content.len()].to_string() }) // TODO should consider chars and not bytes
+                    }
+                } else {
+                }
+            });
+
         // TODO
         diagnostics.push(Diagnostic {
-            range: Range { start: Position { line: 0, character: 0 }, end: Position { line: 0, character: 10 } },
+            range: Range {
+                start: Position {
+                    line: 0,
+                    character: 0,
+                },
+                end: Position {
+                    line: 0,
+                    character: 10,
+                },
+            },
             message: "Zenika rulez!".to_string(),
             severity: Some(DiagnosticSeverity::INFORMATION),
             ..Diagnostic::default()
